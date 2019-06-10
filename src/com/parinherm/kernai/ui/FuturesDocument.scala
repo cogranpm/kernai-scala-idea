c12fd1d8c9d70e8ef8b203d99d1183eaeb3c06ac
mussym <mussym@live.com>
2019-06-10 15:31:09 -0500

    futures stuff

--- src/com/parinherm/kernai/ui/FuturesDocument.scala
+++ src/com/parinherm/kernai/ui/FuturesDocument.scala
@@ -1,5 +1,8 @@
 package com.parinherm.kernai.ui
 
+import  scala.concurrent.{Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class FuturesDocument extends ReferenceDoc {
 
   override def run(): Unit = {
