fe249902dbf8b6e8a573a817f6cd076d7a4ab76c
mussym <mussym@live.com>
2019-06-10 15:37:23 -0500

    futures stuff

--- src/com/parinherm/kernai/ui/FuturesDocument.scala
+++ src/com/parinherm/kernai/ui/FuturesDocument.scala
@@ -2,10 +2,26 @@ package com.parinherm.kernai.ui
 
 import  scala.concurrent.{Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 class FuturesDocument extends ReferenceDoc {
 
   override def run(): Unit = {
     addMessage("Futures")
+    future1()
   }
+
+  def future1() : Unit = {
+    val f = Future {
+
+      "hello futures"
+    }
+    f.onComplete {
+      case Success(value) => addMessage(value)
+      case Failure(e) => addMessage(s"Future failed ${e.getMessage}")
+    }
+
+  }
+
+
 }
